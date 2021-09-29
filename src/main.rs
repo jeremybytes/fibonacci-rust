d66013c0c55dd1cb71fa31d330b195ad0ae73e98
Jeremy Clark <jeremy@jeremybytes.com>
2021-09-29 14:43:41 -0700

    Basic Fibonacci implementation (no error handling)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Fibonacci 1st = {}", fib(1));
+    println!("Fibonacci 2nd = {}", fib(2));
+    println!("Fibonacci 3rd = {}", fib(3));
+    println!("Fibonacci 4th = {}", fib(4));
+    println!("Fibonacci 5th = {}", fib(5));
+}
+
+fn fib(n: u8) -> u64 {
+    let mut prev: u64 = 0;
+    let mut curr: u64 = 1;
+    for _ in 1..n {
+        let next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+    curr
 }
