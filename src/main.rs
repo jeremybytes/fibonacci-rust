c495c25ff2942c9778648678e99b4fa7b1dd95fc
Jeremy Clark <jeremy@jeremybytes.com>
2021-09-29 14:56:20 -0700

    Added checked add to prevent overflow

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let nths = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let nths = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 90, 91, 92, 93, 94, 95, 96];
 
     for nth in nths {
         println!("Fibonacci {} = {}", nth, fib(nth));
@@ -10,9 +10,17 @@ fn fib(n: u8) -> u64 {
     let mut prev: u64 = 0;
     let mut curr: u64 = 1;
     for _ in 1..n {
-        let next = prev + curr;
-        prev = curr;
-        curr = next;
+        let result = prev.checked_add(curr);
+        match result {
+            Some(next) => {
+                prev = curr;
+                curr = next;
+            }
+            None => {
+                curr = 0;
+                break;
+            }
+        }
     }
     curr
 }
