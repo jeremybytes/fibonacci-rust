a4096daf0a830f6149d7f48661e7c548371a035e
sagarishere <5121817+sagarishere@users.noreply.github.com>
2022-08-24 01:56:42 +0300

    Update main.rs
    Adding a small change to account for Fibonacci number function with seed value 0 i.e. argument = 0 and also adding a test for the same in the array nths

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let nths = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 90, 91, 92, 93, 94, 95, 96];
+    let nths = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 90, 91, 92, 93, 94, 95, 96];
 
     for nth in nths {
         match fib(nth) {
@@ -13,6 +13,9 @@ fn fib(n: u8) -> Result<u64, &'static str> {
     let mut prev: u64 = 0;
     let mut curr: u64 = 1;
     let mut overflow = false;
+    if n == 0 {
+        return Ok(0);
+    }
     for _ in 1..n {
         let result = prev.checked_add(curr);
         match result {
